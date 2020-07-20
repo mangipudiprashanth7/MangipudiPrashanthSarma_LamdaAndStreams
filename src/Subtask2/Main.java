65ef02068bcd3cd5e4e95762996f02e70b60a442
Mangipudi Prashanth Sarma <mangipudiprashanth10@gmail.com>
2020-07-20 17:19:26 +0530

    Add files

---
+++ src/Subtask2/Main.java
@@ -0,0 +1,11 @@
+// By Mangipudi Prashanth Sarma
+
+package Subtask2;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Started SubTask2");
+		Alist object = new Alist();
+		object.takeInput();
+	}
+}
